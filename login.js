8d67c815b4aaf9292c23de67755f17772232ba33
simonda181 <94784491+simonda181@users.noreply.github.com>
2022-05-08 23:34:38 +0200

    Add files via upload

---
+++ login.js
@@ -0,0 +1,45 @@
+function myFunction(){
+    window.location.href = "login.html"
+  }
+
+  function myFunction(){
+    window.location.href = "home.html"
+  }
+
+
+
+
+
+
+
+
+
+
+//import { initializeApp } from "firebase/app";
+//import { getDatabase, ref, set } from "firebase/database";
+
+//const firebaseConfig = {
+   // apiKey: "AIzaSyB2hapyzsu95jTrvmtaJkjzWUWMUCUuXo4",
+   // authDomain: "perfectfund2p2investment-22564.firebaseapp.com",
+  //  databaseURL: "https://perfectfund2p2investment-22564-default-rtdb.firebaseio.com",
+  //  projectId: "perfectfund2p2investment-22564",
+   // storageBucket: "perfectfund2p2investment-22564.appspot.com",
+   // messagingSenderId: "1004545540813",
+//appId: "1:1004545540813:web:1de2cf70de5a4e76ccdf84"
+ // };
+  
+  // Initialize Firebase
+ // const app = initializeApp(firebaseConfig);
+
+ // function writeUserData(userId, name, email, imageUrl){
+ // const db = getDatabase();
+ // const reference = ref(db, 'users/' + userId);
+
+ // set(reference,{
+ //     username: name,
+ //     email: email,
+ //     profile_picture : imageUrl
+ // });
+//}
+
+//writeUserData("simonda", "mwala", "simonda@gmail.com", "myimageUrl");
